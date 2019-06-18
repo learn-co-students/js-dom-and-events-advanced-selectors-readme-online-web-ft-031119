51959cee039b379ded4719ec20a9294cfa31bcc2
Daniel Schneider <daschne8@gmail.com>
2019-06-18 16:03:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function lastImageSelector(){
+  // let images =  $('img')
+  // return images[images.length-1]
+  return $('img:last')
+}
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+function divSelector(){
+  return $('div.pics')
+}
+function firstListItem(){
+  return $('ul#pic-list li:first')
+}
